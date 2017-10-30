ceaf55d54bb542086f3721415dd2126b555b39e1
ci-dillon <dillon@codeinstitute.org>
2017-10-30 18:40:21 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get("/", function(req, res) {
+  console.log("Home page requested");
+  res.sendFile(path.join(__dirname + '/home.html'));
+});
+
+app.listen(3000, function() {
+  console.log("listening on port 3000");
+});
