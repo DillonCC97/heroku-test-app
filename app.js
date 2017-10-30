d22903875e5c6818908b1eac0655290af08c825c
ci-dillon <dillon@codeinstitute.org>
2017-10-30 18:57:27 -0400

    fix port for deployment

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + '/home.html'));
 });
 
-app.listen(3000, function() {
-  console.log("listening on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
